3797f36e58d9ca9c9a7e56b373de135a76ad068c
barisceliker1 <36998513+barisceliker1@users.noreply.github.com>
2018-04-26 14:43:26 +0300

    Add files via upload

---
+++ self.py
@@ -0,0 +1,18 @@
+class calisan():
+    personel=[]
+    def __init__(self.isim):
+        self.isim=isim
+        self.kabiliyetler=[]
+        self.personelekle()
+    def personel_ekle(self):
+        self.personel.append(self.isim)
+        print(self.isim)
+    def personel_goruntule(self):
+        print("personel listesi")
+        for kisi in self.personel:
+            print(kisi)
+    def kabiliyet_ekle(self.kabiliyet):
+        self.kabiliyet.append(kabiliyet)
+    def kabiliyet_goruntule(self.isim):
+        for kab in self.kabiliyet:
+            print(kab)
